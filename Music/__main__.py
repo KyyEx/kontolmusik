18152589c9193cb7cc626427547fffec1236691c
ᴷʸʸ <89422473+muhammadrizky16@users.noreply.github.com>
2022-01-09 05:23:44 +0700

    Update __main__.py

--- Music/__main__.py
+++ Music/__main__.py
@@ -1,24 +1,30 @@
-import asyncio
-
+import time
 import pytz
-from apscheduler.schedulers.asyncio import AsyncIOScheduler
-from Music import ASSNAME, BOT_NAME, app, client
-from Music.config import API_HASH, API_ID, AUTO_LEAVE, BOT_TOKEN, LOG_GROUP_ID
-from Music.MusicUtilities.database.functions import clean_restart_stage
-from Music.MusicUtilities.database.queue import get_active_chats, remove_active_chat
-from Music.MusicUtilities.helpers.autoleave import leave_from_inactive_call
-from Music.MusicUtilities.tgcallsrun import run
+import asyncio
+import time
+import uvloop
+import importlib
+from pytz import utc
 from pyrogram import Client
+from Music.config import API_ID, API_HASH, BOT_TOKEN, MONGO_DB_URI, SUDO_USERS, LOG_GROUP_ID, AUTO_LEAVE
+from Music import BOT_NAME, ASSNAME, app, client
+from Music.MusicUtilities.database.functions import clean_restart_stage
+from Music.MusicUtilities.database.queue import (get_active_chats, remove_active_chat)
+from Music.MusicUtilities.tgcallsrun import run
 from pytgcalls import idle
+from motor.motor_asyncio import AsyncIOMotorClient as MongoClient
+from Music.Plugins.autoleave import leave_from_inactive_call
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+
 
 scheduler = AsyncIOScheduler()
 
 Client(
-    ":Music:",
+    ':Music:',
     API_ID,
     API_HASH,
     bot_token=BOT_TOKEN,
-    plugins={"root": "Music.Plugins"},
+    plugins={'root': 'Music.Plugins'},
 ).start()
 
 
@@ -26,10 +32,11 @@ print(f"[INFO]: BOT STARTED AS {BOT_NAME}!")
 print(f"[INFO]: ASSISTANT STARTED AS {ASSNAME}!")
 
 
+
 async def load_start():
     restart_data = await clean_restart_stage()
     if restart_data:
-        print("[INFO]: SENDING RESTART STATUS")
+        print("[INFO]: SENDING RESTART STATUS TO SERVER")
         try:
             await app.edit_message_text(
                 restart_data["chat_id"],
@@ -43,25 +50,25 @@ async def load_start():
         chats = await get_active_chats()
         for chat in chats:
             served_chats.append(int(chat["chat_id"]))
-    except Exception:
+    except Exception as e:
         print("Error came while clearing db")
     for served_chat in served_chats:
         try:
-            await remove_active_chat(served_chat)
-        except Exception:
+            await remove_active_chat(served_chat)                                         
+        except Exception as e:
             print("Error came while clearing db")
+            pass     
     await app.send_message(LOG_GROUP_ID, "Bot Started")
-    print("[INFO]: STARTED")
-
-"""
+    print("[INFO]: STARTED BOT AND SENDING THE INFO TO SERVER")
     if AUTO_LEAVE:
         print("[ INFO ] STARTING SCHEDULER")
         scheduler.configure(timezone=pytz.utc)
-        scheduler.add_job(self.leave_from_inactive_call, "interval", seconds=AUTO_LEAVE)
-        scheduler.start()
-"""
-
-
+        scheduler.add_job(
+            leave_from_inactive_call, "interval", seconds=AUTO_LEAVE
+        )
+        scheduler.start()    
+    
+   
 loop = asyncio.get_event_loop()
 loop.run_until_complete(load_start())
 
